95eda61efa37efc89bb0ac1efc8cb462c16ecafc
EnderASz <olek5188@gmail.com>
2021-12-16 10:20:21 +0100

    Fixed import error and next lined __all__ list

--- Guiden/subapps/__init__.py
+++ Guiden/subapps/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 
-import home
+from . import home
 
 subapps_blueprint = Blueprint('subapps', __name__)
 
 subapps_blueprint.register_blueprint(home.home_blueprint)
 
-__all__ = ["home"]
+__all__ = [
+    "home",
+]
