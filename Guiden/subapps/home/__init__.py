555c79609c0a24f177df9c386607ac1496bddf42
EnderASz <olek5188@gmail.com>
2022-01-06 19:29:25 +0100

    Temporary remove SCSS support for home subapp static route and static route itself

--- Guiden/subapps/home/__init__.py
+++ Guiden/subapps/home/__init__.py
@@ -1,23 +1,11 @@
-from flask import Blueprint, current_app
-
-from sassutils.wsgi import SassMiddleware
+from flask import Blueprint
 
 from .views import home as home_view
 
 home_blueprint = Blueprint(
     "home",
     __name__,
-    template_folder="templates",
-    static_folder="static",
-    static_url_path="/static/home")
-
-if current_app.config.get('ENV') != 'production':
-        home_blueprint.wsgi_app = SassMiddleware(
-            home_blueprint.wsgi_app,
-            {
-                'Guiden': ('static/scss', 'static/css', '/static/home/css', False),
-            }
-        )
+    template_folder="templates")
 
 
 home_blueprint.add_url_rule('/', view_func=home_view)
