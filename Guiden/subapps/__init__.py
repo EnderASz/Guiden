bfb18a2a4a5fd2cf172ecedd905e577ca8337253
EnderASz <olek5188@gmail.com>
2022-01-29 17:56:47 +0100

    Init auth subapp with login page

--- Guiden/subapps/__init__.py
+++ Guiden/subapps/__init__.py
@@ -1,11 +1,15 @@
 from flask import Blueprint
 
 from . import home
+from . import auth
+
 
 subapps_blueprint = Blueprint('', __name__)
 
 subapps_blueprint.register_blueprint(home.home_blueprint)
+subapps_blueprint.register_blueprint(auth.auth_blueprint, url_prefix="/auth")
 
 __all__ = [
     "home",
+    "auth",
 ]
